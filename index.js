a42e608369cb7974a97c8480666db6dce4da3c75
gvr9094 <98337647+gvr9094@users.noreply.github.com>
2022-04-11 10:32:26 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 var inputLocation = document.getElementById('searchBar');
 var buttomSubmit =  document.getElementById('.btn');
-var locationValue =  document.getElementById('.location');
-var descValue =  document.getElementById('.desc'); 
-var tempValue =  document.getElementById('.temp');
+var cityValue =  document.getElementById('city');
+var descValue =  document.getElementById('desc'); 
+var tempValue =  document.getElementById('temp');
+var humValue =  document.getElementById('humidity');
+var pressValue =  document.getElementById('pressure');
+var countryValue =  document.getElementById('country');
  btn.addEventListener('click' , function(){
      fetch('https://api.openweathermap.org/data/2.5/weather?q='+searchBar.value+'&appid=1e8ad8209fb1c5d934f4f0355f92f807')
      .then(response => response.json())
      .then(data =>{
-         var locationDisplay = data['searchBar'];
+     
+         var cityDisplay = data['name'];
          var tempDisplay = data['main']['temp'];
          var descDisplay = data['weather'][0]['description'];
-        
+         var humidityDisplay = data['main']['humidity'];
+         var pressureDisplay = data['main']['pressure'];
+         var countryDisplay = data['sys']['country'];
 
-         searchBar.value.innerHTML =locationDisplay;
+
+
+         humidity.innerHTML = '<br>'+'Humidity'+'&nbsp;'+':'+'&nbsp;'+humidityDisplay;
+         pressure.innerHTML = '<br>'+'Pressure'+'&nbsp;'+':'+'&nbsp;'+pressureDisplay;
+         city.innerHTML = cityDisplay;
+         country.innerHTML ='<br>'+'Country'+'&nbsp;'+':'+'&nbsp;'+countryDisplay;
          temp.innerHTML ='<br>'+'Temperature'+'&nbsp;'+':'+'&nbsp;'+tempDisplay;
          desc.innerHTML = '<br>'+'Description'+'&nbsp;'+':'+'&nbsp;'+ descDisplay;
 
